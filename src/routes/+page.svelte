48cb297cb1c7c5189e577e6e4e362757341f9263
Jake Jasmin <roastbeefer000@gmail.com>
2023-05-20 18:16:56 -0600

    add Ron, fix home on S20

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,12 +10,12 @@
     let current_date = new Date();
 </script>
 
-<section class="container p-3">
+<section class="container p-1">
   <div class="notification mx-auto is-dark">
       <h1 class="title is-2 has-text-centered">Welcome to Cruces Chess Club!</h1>
   </div>
   <div class="column is-desktop is-half is-offset-one-quarter">
-    <div class="box m-3">
+    <div class="box m-1">
         <Calendar month={current_date.getMonth()} />
     </div>
   </div>
