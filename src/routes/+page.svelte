6ff7c4c01a2e933a97e77ce93e15dc550bbc13e7
Jake Jasmin <roastbeefer000@gmail.com>
2023-07-23 14:57:10 -0600

    add affiliate logo home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,12 +11,29 @@
 </script>
 
 <section class="container p-1">
+  <!-- Greeting -->
   <div class="notification mx-auto is-dark">
       <h1 class="title is-2 has-text-centered">Welcome to Cruces Chess Club!</h1>
   </div>
+  
+  <!-- Calendar -->
   <div class="column is-desktop is-half is-offset-one-quarter">
     <div class="box m-1">
         <Calendar month={current_date.getMonth()} />
     </div>
   </div>
+
+  <!-- Bottom banner -->
+  <div class="navbar is-flex-touch is-fixed-bottom">
+      <a class="mx-auto" href="https://www.chess.com/club/las-cruces-chess">
+          <img class="mx-auto" alt="bottom banner" src="affiliate/Digital Leaderboard Ad.png">
+      </a>
+
+  </div>
 </section>
+
+<style>
+    .navbar {
+        background-color: hsl(0 100% 100% / 0.0);
+    }
+</style>
