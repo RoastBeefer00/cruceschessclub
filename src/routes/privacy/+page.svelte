38abdbea075b1fc7d2161f1385fde1c01211f936
Jake <jake@mac.lan>
2025-04-11 20:16:06 -0600

    privacy

---
+++ src/routes/privacy/+page.svelte
@@ -0,0 +1,47 @@
+<div class="container block p-3">
+
+    <article class="message">
+    <div class="message-header">
+    <h1>Privacy Policy for Cruces Chess Club</h1>
+    <p>Effective Date: April 11, 2025</p>
+    </div>
+    <div class="message-body">
+    <p>At Cruces Chess Club, we are committed to protecting your privacy. This Privacy Policy outlines our practices regarding personal data.</p>
+    <br />
+    <p>Data Collection:</p>
+
+    <p>We want to clearly state that Cruces Chess Club does not collect any personal data from its members, visitors, or website users. This includes, but is not limited to:</p>
+
+    <br />
+    <ul>
+        <li>Names</li>
+        <li>Email addresses</li>
+        <li>Phone numbers</li>
+        <li>Addresses</li>
+        <li>IP addresses</li>
+        <li>Browsing history</li>
+        <li>Any other identifiable information</li>
+    </ul>
+
+    <br />
+    <p>How We Operate:</p>
+    <p>Cruces Chess Club functions as a gathering place for individuals interested in playing chess. We primarily operate through in-person meetings and may utilize online platforms for informational purposes only, without requiring or storing any personal information.</p>
+
+    <br />
+    <p>Third-Party Links:</p>
+
+    <p>Our website or communications may occasionally contain links to third-party websites. Please note that we are not responsible for the privacy practices of these external sites. We encourage you to review the privacy policies of any website you visit.  </p> 
+
+    <br />
+    <p>Changes to This Policy:</p>
+
+    <p>We may update this Privacy Policy from time to time. Any changes will be posted on our website or communicated through other appropriate channels. The effective date at the top of this policy will indicate the latest revision.  </p> 
+
+    <br />
+    <p>Contact Us:</p>
+
+    <p>If you have any questions about this Privacy Policy, please feel free to contact us through our general contact information (e.g., at a club meeting or via a publicly available contact method if you have one).</p>
+    </div>
+
+    </article>
+</div>
