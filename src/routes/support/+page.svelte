e5ff12d42317116a505f4470be2a78ca27211a43
Jake Jasmin <roastbeefer000@gmail.com>
2025-07-17 16:37:21 -0600

    support

---
+++ src/routes/support/+page.svelte
@@ -0,0 +1,14 @@
+<div class="container block p-3">
+  <article class="message">
+    <div class="message-header">
+      <h1>Support</h1>
+    </div>
+    <div class="message-body">
+      <p>For support, please reach out to either Isaac or Jake at:</p>
+      <ul>
+        <li>isaac@isaachimself.com</li>
+        <li>roastbeefer000@gmail.com</li>
+      </ul>
+    </div>
+  </article>
+</div>
